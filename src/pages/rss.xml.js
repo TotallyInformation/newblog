4ce98bea39664c07ff36aa794d56cd7346d0a16b
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2024-10-23 20:31:03 +0100

    Fix the early broken bits

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function GET(context) {
         title: 'Totally Information | Blog',
         description: 'All things related to Digital Technology and Information, and perhaps some social and moral commentary as well',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: await pagesGlobToRssItems(import.meta.glob(['./**/*.md', './**/*.mdx', '!./**/_*.md', '!./**/_*.mdx',])),
         customData: `<language>en-gb</language>`,
     });
 }
