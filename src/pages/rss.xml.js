31bb74c9b9823a32ee1dc5925e470dd31a4129e7
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2024-10-24 21:15:11 +0100

    More alpha updates

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,34 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import siteInfo from '@/site-info.json';
+import { getAllArticles } from '@/utilModules/getCollections.mjs'
+const allItems = await getAllArticles();
+// console.log('[/src/pages/rss.xml.js] allItems length: ', allItems.length, allItems[0])
+// const { Content, headings } = await allItems[0].render();
+// console.log('[/src/pages/rss.xml.js] allItems html: ', Content)
+
+const rssItems = allItems.map((post) => {
+    // const { Content, headings } = await post.render();
+    const cats = post.data?.category ?? []
+    const tags = post.data?.tags ?? []
+    const allCats = cats.concat(tags)
+    return {
+        title: post.data.title,
+        pubDate: post.data.updated,
+        description: post.data.description ?? post.data.title,
+        link: post.data.path,
+        categories: allCats,
+        author: post.data?.author ?? siteInfo.author,
+        // content: {<Content />}
+    }
+});
+console.log('[/src/pages/rss.xml.js] rssItems length: ', rssItems.length, rssItems[0])
 
 export async function GET(context) {
     return rss({
-        title: 'Totally Information | Blog',
-        description: 'All things related to Digital Technology and Information, and perhaps some social and moral commentary as well',
+        title: `${siteInfo.title} | Latest Articles`,
+        description: siteInfo.description,
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob(['./**/*.md', './**/*.mdx', '!./**/_*.md', '!./**/_*.mdx',])),
         customData: `<language>en-gb</language>`,
+        items: rssItems,
     });
 }
